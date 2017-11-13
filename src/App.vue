9b2b3062490e7ff9c8522ac862cd484871201da0
huaixiu.zhen <huaixiu.zhen@gmail.com>
2017-11-13 13:40:45 +0800

    fix some css bug

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,33 @@
         currentStyle: '',
         enableHtml: false,
         fullStyle: [
-          `/*
+          `/**
 * Everybody yo yo ,come let's go !
 * 大家好,我是甄怀修,一个要成为网红的程序员
 * 喜欢前端的PHP后端工程师
 * 下面我来写个页面简单介绍一下自己
 */
 
-/* 首先给所有元素加上过渡效果 */
+/**
+ *  首先给所有元素加上过渡效果
+ */
 * {
   transition: all 1s;
   -webkit-transition: all 1s;
 }
 
-/* 白色背景太刺眼,换一个原谅绿*/
+/**
+ * 白色背景太刺眼,换一个原谅绿
+ */
 html {
   background: rgba(101, 234, 45, 0.59);
+  /* Emmm.. It doesn't seem to fit me */
+  background: rgb(63, 82, 99);
 }
 
-/* ok,美化一下我们的工作区 */
+/**
+ * ok,美化一下我们的工作区
+ */
 .style-div {
   padding: 10px 10px 20px;
   overflow: auto;
@@ -52,109 +60,128 @@ html {
   font-size: 14px;
   box-shadow: -4px 4px 2px 0 rgba(0,0,0,0.3);
 }
-
 .style-div:hover {
   box-shadow: 0px 0px 40px 5px rgba(255,255,255,0.4);
 }
 
-/* 代码看着不舒服,加个高亮 */
-.token.comment{ color: #857F6B; font-style: italic; }
-.token.selector{ color: #E69F0F }
-.token.property{ color: #64D5EA; }
-.token.punctuation{ color: #ddd; }
-.token.function{ color: #BE84F2; }
+/**
+ * 代码看着不舒服,加个高亮
+ */
+.token.comment { color: #857F6B; font-size: 12px; font-style: italic; }
+.token.selector { color: #E69F0F }
+.token.property { color: #64D5EA; }
+.token.punctuation { color: #ddd; }
+.token.function { color: #BE84F2; }
 
-/* 加一些3D转换效果 */
+/**
+ * 加一些3D转换效果
+ */
 .style-div {
   position: fixed; left: 10px; top: 10px;
-  transform: rotateY(10deg) translateZ(-100px) ;
-  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+  transform: rotateY(10deg) translateZ(-100px);
+  -webkit-transform: rotateY(10deg) translateZ(-100px);
 }
 
-/* 改变一下可视角度 */
-html{
+/**
+ * 改变一下可视角度
+ */
+html {
   perspective: 1500px;
-  -webkit-perspective: 1500px; /* Safari 和 Chrome */
+  -webkit-perspective: 1500px;
 }
 
-/* 接下来我给自己准备一个编辑器 */
-.resume-div{
-  position: fixed; right: 0; top: 0;
-  padding: .5em;  margin: .5em;
+/**
+ * Okey,我还需要一个填充内容的容器
+ */
+.resume-div {
+  position: fixed; right: 10px; top: 10px;
+  padding: 10px; margin: 10px;
   width: 48vw; height: 90vh;
   border: 1px solid;
   background: white; color: #222;
   overflow: auto;
+  transform: rotateY(-10deg) translateZ(-100px);
+  -webkit-transform: rotateY(-10deg) translateZ(-100px);
 }
-/* 好了，我开始写简历了 */
+
+/**
+ * 好，开始写内容
+ */
 
 
 `,
-          `
-/* 这个简历好像差点什么
- * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
- * 简单，用开源工具翻译成 HTML 就行了
+`
+/**
+ * 把 Markdown 格式转换成 HTML
  */
 `
-          ,
-          `
+,
+`
 /* 再对 HTML 加点样式 */
-.resume-div{
-  padding: 2em;
+.resume-div {
+  padding: 10px;
 }
-.resume-div h2{
+.resume-div h2 {
   display: inline-block;
   border-bottom: 1px solid;
-  margin: 1em 0 .5em;
+  margin: 20px 0 10px;
 }
-.resume-div ul,.resume-div ol{
+.resume-div ul,.resume-div ol {
   list-style: none;
 }
-.resume-div ul> li::before{
+.resume-div ul> li::before {
   content: '•';
-  margin-right: .5em;
+  margin-right: 10px;
+}
+.resume-div ul> li {
+   margin: 10px;
 }
 .resume-div ol {
   counter-reset: section;
 }
-.resume-div ol li::before {
-  counter-increment: section;
-  content: counters(section, ".") " ";
-  margin-right: .5em;
-}
-.resume-div blockquote {
-  margin: 1em;
-  padding: .5em;
-  background: #ddd;
-}
+/** OK,还有最后一句话
+ * PHP是世界上最好的语言
+ */
 `],
         currentMarkdown: '',
-        fullMarkdown: `甄怀修
+        fullMarkdown: `
+## 甄怀修
 ----
 
-PHP,MYSQL,LINUX工程师
+- 手机：134 - hello - world;
+- Email：huaixiu.zhen@gmail.com;
+- 个人技术博客：https://www.litblc.com;
 
-技能
+## 基本信息
 ----
 
-* php
-* Rails 开发
-* Node.js 开发
-* linux
+- 甄怀修 / 男 / 1992;
+- 中南民族大学 / 本科;
+- PHP 开发工程师 / 深圳;
+- 喜欢探索新技术 / 自学PHP已达四年时间
 
-工作经历
+
+## 技能
 ----
 
-1.
-2.
-3.
-4.
+* LINUX | GIT | NGINX | APACHE | DOCKER
+* COMPOSER | NPM | WEBPACK | GULP
+* PHP | MYSQL | LARAVEL | SYMFONY
+* VUE | JQUERY | BOOTSTRAP
 
-链接
+## 工作经历
 ----
 
-* [GitHub](https://github.com/shyZhen)
-* [我的博客](https://www.litblc.com)
+- 深圳市天英联合教育股份有限公司  南山智园 2017/06 - 至今
+- 深圳市蓝网科技股份有限公司  南山智园 2016/09 - 2017/06
+
+
+## 链接
+----
+
+* [个人导航|HTML5](http://phpb.io)
+* [GitHub|shyZhen](https://github.com/shyZhen)
+* [我的博客|全站SSL全球CDN七牛静态资源分发](https://www.litblc.com)
 
 
 `
--- src/Mobile.vue
+++ src/Mobile.vue
@@ -17,219 +17,243 @@
       ResumeEditor
     },
     data() {
-      return {
-        interval: 40,
-        currentStyle: '',
-        enableHtml: false,
-        fullStyle: [
-          `/*
-* Inspired by http://strml.net/
-* 大家好，我是方方
-* 二月了，好多公司都在招聘
-* 你是不是也在准备简历呀。
-* 说做就做，我也来写一份简历！
-*/
+    return {
+      interval: 40,
+      currentStyle: '',
+      enableHtml: false,
+      fullStyle: [
+      `/**
+ * Everybody yo yo ,come let's go !
+ * 大家好,我是甄怀修,一个要成为网红的程序员
+ * 喜欢前端的PHP后端工程师
+ * 下面我来写个页面简单介绍一下自己
+ */
 
-/* 首先给所有元素加上过渡效果 */
+/**
+ *  首先给所有元素加上过渡效果
+ */
 * {
-  transition: all .3s;
+  transition: all 1s;
+  -webkit-transition: all 1s;
 }
-/* 白色背景太单调了，我们来点背景 */
+
+/**
+ * 白色背景太刺眼,换一个原谅绿
+ */
 html {
-  color: rgb(222,222,222);
-  background: rgb(0,43,54);
+  background: rgba(101, 234, 45, 0.59);
+  /* Emmm.. It doesn't seem to fit me */
+  background: rgb(63, 82, 99);
 }
-/* 文字离边框太近了 */
-.styleEditor {
-  padding: .5em;
-  border: 1px solid;
+
+/**
+ * ok,美化一下我们的工作区
+ */
+.style-div {
+  padding: 10px 10px 20px;
   overflow: auto;
-  width: 90vw;
-  margin: 2.5vh 5vw;
+  background: rgb(48, 48, 48);
+  color: white;
+  border: 1px solid #ccc;
+  width: 95vw;
   height: 90vh;
+  font-size: 14px;
+  box-shadow: -4px 4px 2px 0 rgba(0,0,0,0.3);
 }
-/* 太高了 */
-.styleEditor {
-  height: 45vh;
-}
-/* 代码高亮 */
-.token.selector{
-  color: rgb(133,153,0);
-}
-.token.property{
-  color: rgb(187,137,0);
-}
-.token.punctuation{
-  color: yellow;
-}
-.token.function{
-  color: rgb(42,161,152);
+.style-div:hover {
+  box-shadow: 0px 0px 40px 5px rgba(255,255,255,0.4);
 }
 
-/* 加点 3D 效果呗 */
-html{
-  perspective: 1000px;
-}
-.styleEditor {
-  position: fixed; left: 0; top: 0;
-  transform: rotateX(-10deg) translateZ(-50px) ;
+/**
+ * 代码看着不舒服,加个高亮
+ */
+.token.comment { color: #857F6B; font-size: 12px; font-style: italic; }
+.token.selector { color: #E69F0F }
+.token.property { color: #64D5EA; }
+.token.punctuation { color: #ddd; }
+.token.function { color: #BE84F2; }
+
+/**
+ * 加一些3D转换效果
+ */
+.style-div {
+  position: fixed; left: 10px; top: 10px;
+  transform: rotateY(10deg) translateZ(-100px);
+  -webkit-transform: rotateY(10deg) translateZ(-100px);
 }
 
-/* 接下来我给自己准备一个编辑器 */
-.resumeEditor{
-  position: fixed;
-  top: 50%; left: 0;
-  padding: .5em;  margin: 2.5vh;
-  width: 95vw; height: 45vh;
+/**
+ * 改变一下可视角度
+ */
+html {
+  perspective: 1500px;
+  -webkit-perspective: 1500px;
+}
+
+/**
+ * Okey,我还需要一个填充内容的容器
+ */
+.resume-div {
+  position: fixed; right: 10px; top: 50%;
+  padding: 10px; margin: 10px;
+  width: 100vw;
+  height: 70vh;
   border: 1px solid;
   background: white; color: #222;
   overflow: auto;
+  transform: rotateY(-10deg) translateZ(-100px);
+  -webkit-transform: rotateY(-10deg) translateZ(-100px);
 }
-/* 好了，我开始写简历了 */
+
+/**
+ * 好，开始写内容
+ */
 
 
 `,
-          `
-/* 这个简历好像差点什么
- * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
- * 简单，用开源工具翻译成 HTML 就行了
+`
+/**
+ * 把 Markdown 格式转换成 HTML
  */
 `
-          ,
-          `
-/* 再对 HTML 加点样式 */
-.resumeEditor{
-  padding: 2em;
-}
-.resumeEditor h2{
-  display: inline-block;
-  border-bottom: 1px solid;
-  margin: 1em 0 .5em;
-}
-.resumeEditor ul,.resumeEditor ol{
-  list-style: none;
-}
-.resumeEditor ul> li::before{
-  content: '•';
-  margin-right: .5em;
-}
-.resumeEditor ol {
-  counter-reset: section;
-}
-.resumeEditor ol li::before {
-  counter-increment: section;
-  content: counters(section, ".") " ";
-  margin-right: .5em;
-}
-.resumeEditor blockquote {
-  margin: 1em;
-  padding: .5em;
-  background: #ddd;
-}
-`],
-        currentMarkdown: '',
-        fullMarkdown: `方应杭
-----
-
-资深前端工程师，资深前端讲师
-现在在 [饥人谷](http://jirengu.com) 教前端课程。
-
-技能
-----
-
-* 前端开发
-* Rails 开发
-* Node.js 开发
-* 前端授课
-
-工作经历
-----
-
-1. [饥人谷](http://jirengu.com)
-2. 腾讯即时通讯平台部
-3. 阿里巴巴B2B部门
-4. 彩程知人项目组
-
-链接
-----
-
-* [GitHub](https://github.com/frankfang)
-* [我的文章](https://www.zhihu.com/people/zhihusucks/pins/posts)
-
-> 如果你喜欢这个效果，
-Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你自己的简历！
-
+,
 `
-      }
-    },
-    created() {
-      this.makeResume()
-    },
+/**
+ * 再对 HTML 加点样式
+ */
+.resume-div {
+    padding: 10px;
+  }
+.resume-div h2 {
+    display: inline-block;
+    border-bottom: 1px solid;
+    margin: 20px 0 10px;
+  }
+.resume-div ul,.resume-div ol {
+    list-style: none;
+  }
+.resume-div ul> li::before {
+    content: '•';
+    margin-right: 10px;
+  }
+.resume-div ul> li {
+    margin: 10px;
+  }
+.resume-div ol {
+    counter-reset: section;
+  }
+/** OK,还有最后一句话
+* PHP是世界上最好的语言
+*/
+`],
+  currentMarkdown: '',
+  fullMarkdown: `
+## 甄怀修
+----
 
-    methods: {
-      makeResume: async function () {
-        await this.progressivelyShowStyle(0)
-        await this.progressivelyShowResume()
-        await this.progressivelyShowStyle(1)
-        await this.showHtml()
-        await this.progressivelyShowStyle(2)
-      },
-      showHtml: function () {
-        return new Promise((resolve, reject) => {
-          this.enableHtml = true
+- 手机：134 - hello - world;
+- Email：huaixiu.zhen@gmail.com;
+- 个人技术博客：https://www.litblc.com;
+
+## 基本信息
+----
+
+- 甄怀修 / 男 / 1992;
+- 中南民族大学 / 本科;
+- PHP 开发工程师 / 深圳;
+- 喜欢探索新技术 / 自学PHP已达四年时间
+
+## 技能
+----
+
+* LINUX | GIT | NGINX | APACHE | DOCKER
+* COMPOSER | NPM | WEBPACK | GULP
+* PHP | MYSQL | LARAVEL | SYMFONY
+* VUE | JQUERY | BOOTSTRAP
+
+## 工作经历
+----
+
+- 深圳市天英联合教育股份有限公司  南山智园 2017/06 - 至今
+- 深圳市蓝网科技股份有限公司  南山智园 2016/09 - 2017/06
+
+
+## 链接
+----
+
+* [个人导航|HTML5](http://phpb.io)
+* [GitHub|shyZhen](https://github.com/shyZhen)
+* [我的博客|全站SSL全球CDN七牛静态资源分发](https://www.litblc.com)
+
+
+  `
+  }
+  },
+  created() {
+    this.makeResume()
+  },
+
+  methods: {
+    makeResume: async function () {
+      await this.progressivelyShowStyle(0)
+      await this.progressivelyShowResume()
+      await this.progressivelyShowStyle(1)
+      await this.showHtml()
+      await this.progressivelyShowStyle(2)
+    },
+    showHtml: function () {
+      return new Promise((resolve, reject) => {
+        this.enableHtml = true
+      resolve()
+    })
+  },
+  progressivelyShowStyle(n) {
+    return new Promise((resolve, reject) => {
+              let interval = this.interval
+              let showStyle = (async function () {
+      let style = this.fullStyle[n]
+      if (!style) { return }
+      // 计算前 n 个 style 的字符总数
+      let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+      let prefixLength = length - style.length
+      if (this.currentStyle.length < length) {
+        let l = this.currentStyle.length - prefixLength
+        let char = style.substring(l, l + 1) || ' '
+        this.currentStyle += char
+        if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
           this.$nextTick(() => {
-            this.$refs.resumeEditor.goTop()
-          })
-          resolve()
-        })
-      },
-      progressivelyShowStyle(n) {
-        return new Promise((resolve, reject) => {
-          let interval = this.interval
-          let showStyle = (async function () {
-            let style = this.fullStyle[n]
-            if (!style) { return }
-            // 计算前 n 个 style 的字符总数
-            let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
-            let prefixLength = length - style.length
-            if (this.currentStyle.length < length) {
-              let l = this.currentStyle.length - prefixLength
-              let char = style.substring(l, l + 1) || ' '
-              this.currentStyle += char
-              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
-                this.$nextTick(() => {
-                  this.$refs.styleEditor.goBottom()
-                })
-              }
-              setTimeout(showStyle, interval)
-            } else {
-              resolve()
-            }
-          }).bind(this)
-          showStyle()
-        })
-      },
-      progressivelyShowResume() {
-        return new Promise((resolve, reject) => {
-          let length = this.fullMarkdown.length
-          let interval = this.interval
-          let showResume = () => {
-            if (this.currentMarkdown.length < length) {
-              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
-              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
-              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
-              if (prevChar === '\n' && this.$refs.resumeEditor) {
-                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
-              }
-              setTimeout(showResume, interval)
-            } else {
-              resolve()
-            }
-          }
-          showResume()
+            this.$refs.styleEditor.goBottom()
         })
       }
+      setTimeout(showStyle, interval)
+    } else {
+      resolve()
     }
+  }).bind(this)
+  showStyle()
+  })
+  },
+  progressivelyShowResume() {
+    return new Promise((resolve, reject) => {
+              let length = this.fullMarkdown.length
+              let interval = this.interval
+              let showResume = () => {
+              if (this.currentMarkdown.length < length) {
+      this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+      let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+      let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+      if (prevChar === '\n' && this.$refs.resumeEditor) {
+        this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+      }
+      setTimeout(showResume, interval)
+    } else {
+      resolve()
+    }
+  }
+  showResume()
+  })
+  }
+  }
   }
 
 </script>
@@ -239,7 +263,6 @@ Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    min-height: 100vh; position: relative;
   }
 
   html {
@@ -248,5 +271,4 @@ Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你
   *{
     box-sizing: border-box;
   }
-
 </style>
