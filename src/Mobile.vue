b740c5d2fb7849545b88a6fe00cf835787272bd6
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-05-31 10:09:45 +0800

    修改学院内容

--- src/Mobile.vue
+++ src/Mobile.vue
@@ -18,28 +18,32 @@
     },
     data() {
       return {
-        interval: 4,
+        interval: 2,
         currentStyle: '',
         enableHtml: false,
         fullStyle: [
           `/*
-* Inspired by http://strml.net/
-* 大家好，我是方方
-* 二月了，好多公司都在招聘
-* 你是不是也在准备简历呀。
-* 说做就做，我也来写一份简历！
+* 同学你好,这里是
+* 东南大学
+* 计算机科学与工程学院、
+* 软件学院、人工智能学院。
+*/
+
+/*
+* 一起来看看你的学长学姐们
+* 是怎么做推送的吧～
 */
 
 /* 首先给所有元素加上过渡效果 */
 * {
   transition: all .3s;
 }
-/* 白色背景太单调了，我们来点背景 */
+/* 白色背景太单调了,我们来点背景 */
 html {
   color: rgb(222,222,222);
   background: rgb(0,43,54);
 }
-/* 文字离边框太近了 */
+/* 调整一下代码框的位置 */
 .styleEditor {
   padding: .5em;
   border: 1px solid;
@@ -48,7 +52,7 @@ html {
   margin: 2.5vh 5vw;
   height: 90vh;
 }
-/* 代码高亮 */
+/* 代码高亮才不容易出错 */
 .token.selector{
   color: rgb(133,153,0);
 }
@@ -76,37 +80,73 @@ html{
   transform: rotateX(-30deg) translateZ(0px) ;
 }
 
-/* 接下来我给自己准备一个编辑器 */
+/* 接下来,正文就要登场了 */
 .resumeEditor{
   position: fixed;
   top: 21%; left: 0;
   padding: .5em;  margin: 3vh;
   width: 90vw; height: 72vh;
   border: 1px solid;
-  background: white; color: #222;
+  background: #FCFFDB; color: #222;
   overflow: auto;
+  border-radius: 8px;
 }
-/* 好了，我开始写简历了 */
+/* 一起来看看我们的学院介绍吧！ */
 
 
 `,
           `
-/* 这个简历好像差点什么
- * 对了，这是 Markdown 格式的，我需要变成对 HR 更友好的格式
- * 简单，用开源工具翻译成 HTML 就行了
+/* 这个介绍好像差点什么
+ * 对了,这是 Markdown 格式的,我需要变成对 萌新们 更友好的格式
+ * 简单,用开源工具翻译成 HTML 就行了
  */
 `
           ,
           `
-/* 再对 HTML 加点样式 */
+/* 格式还需要优化 */
 .resumeEditor{
   padding: 2em;
 }
+
+/* 标题太大了 */
+.resumeEditor h1{
+  font-size:24px;
+}
+
+/* 副标题也太大了 */
 .resumeEditor h2{
   display: inline-block;
-  border-bottom: 1px solid;
   margin: 1em 0 .5em;
+  font-size: 20px;
+  color: #007419;
 }
+
+/* 文本小一些显得精致 */
+.resumeEditor p{
+  font-size: 15px;
+  margin-bottom: 0.5em;
+}
+
+.resumeEditor p strong{
+  color:#003174;
+}
+
+/* 图片也需要圆角和阴影 */
+.resumeEditor img {
+  width: 100%;
+  border-radius: 5px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  margin-top: 10px;
+}
+
+.resumeEditor li{
+  font-size: 15px;
+}
+
+.resumeEditor li strong{
+  color:#003174;
+}
+
 .resumeEditor ul,.resumeEditor ol{
   list-style: none;
 }
@@ -127,39 +167,119 @@ html{
   padding: .5em;
   background: #ddd;
 }
+
 `],
         currentMarkdown: '',
-        fullMarkdown: `方应杭
-----
+        fullMarkdown: `
 
-资深前端工程师，资深前端讲师
-现在在 [饥人谷](http://jirengu.com) 教前端课程。
+# 东南大学计算机科学与工程学院、软件学院、人工智能学院
 
-技能
-----
+## 计算机科学与工程学院简介
 
-* 前端开发
-* Rails 开发
-* Node.js 开发
-* 前端授课
 
-工作经历
-----
+学院起源于1960年建立的
+“解算装置及技术”专业,
+曾自主研制我国第一台数字积分机,
+填补了国内空白,
+并成功地应用于国防现代化和工业自动化领域,
+取得了开创性成果。
+现有
+**“计算机科学与技术”**
+和
+**“软件工程”**
+两个一级学科博士点,
+其中 “计算机应用技术”
+二级学科博士点为**国家重点学科**。
+在ESI学科排名中,
+计算机学科已进入**全球前1‰**
+(截止目前,列全球第22位,国内第二位)。
+计算机科学与技术一级学科为
+**“双一流”国家重点建设学科**,
+软件工程一级学科于2018年
+进入**江苏省优势学科**立项建设范围。
+近五年来,获国家教学成果一等奖、
+江苏省教学成果一等奖、
+省优秀教学成果二等奖、
+中国学位与研究生教育学会二等奖、
+全国高等学校计算机实践教学一等奖各1项。
+“计算机教学实验中心”是
+国家及江苏省教学实验示范中心建设点,
+“数据库原理共享课”为国家级精品课程。
 
-1. [饥人谷](http://jirengu.com)
-2. 腾讯即时通讯平台部
-3. 阿里巴巴B2B部门
-4. 彩程知人项目组
+## 软件学院简介
+学院成立于2001年12月,
+是首批成立的**“国家示范性软件学院”**
+依托“软件工程”和“计算机科学与技术”
+两个一级学科开展办学,其中,
+“软件工程”学科是在“计算机软件与理论”
+江苏省**重点学科**基础上发展形成的。
+该学科于1986年获硕士学位授予权,
+2003年获博士学位授予权,
+2006年成为江苏省重点学科,
+2012年成为江苏省一级学科重点学科。
+学院始终以**现代工程教育**的理念为指引
+建立创新人才培养模式,
+并成为学校**工程教育教学改革**的先行者。
 
-链接
-----
+## 人工智能学院简介
+学院以计算机科学与工程学院为基础,
+以学校相关专业为依托发展而来,成立于2018年,
+是国家**首批申报并设立**“人工智能”专业的学院之一。
+2018年秋季依托计算机科学与工程学院
+招收了第一届人工智能专业本科生。
+学院在**人工智能基础理论**和
+**应用研究**方面具有深厚的研究基础,
+主要研究方向包括**机器学习**、
+**知识表示与推理**、
+**计算机视觉**、
+**自然语言处理**、
+**多智能体**、**图像处理**、**科学可视化**等。
+学院集中了计算机科学与技术、
+控制科学与工程、
+信息与通信工程等
+多个国家**“双一流”**学科所在学院的
+优势科研团队和科研资源,将在学科地位、
+科研水平、
+教学质量、
+人才培养等方面建设国内外
+具有**明显特色**和**较高学术水平**的人工智能专业。
 
-* [GitHub](https://github.com/frankfang)
-* [我的文章](https://www.zhihu.com/people/zhihusucks/pins/posts)
+## 师资队伍
+学院现有专任教师93人,
+拥有一支**结构合理、充满朝气、
+以中青年教师为主的**高水平师资队伍。其中,
+* 中国工程院院士 **1** 人
+* 博士生导师 **22** 人
+* 新世纪“百千万人才工程”国家级人选 **1** 人
+* 长江学者特聘教授 **1** 人
+* 青年长江特聘教授 **1** 人
+* 获国家自然科学基金优秀青年科学基金 **2** 人
+* 教育部跨世纪人才 **2** 人
+* 教育部新世纪优秀人才 **6** 人
+* 教育部优秀青年教师 **2** 人
+* 江苏省“333人才工程”培养对象 **6** 人
+* 江苏省“六大人才高峰” **10** 人
+* 江苏省“青蓝工程”培养对象 **8** 人
+* 江苏省教育系统先进工作者 **1** 人
+* 江苏省“双创人才” **1** 人
+* 江苏省“双创博士”资助对象 **3** 人
+* “长江学者和创新团队发展计划”创新团队 **1** 个
 
-> 如果你喜欢这个效果，
-Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你自己的简历！
+## 学生活动
 
+学院非常重视第二课堂的建设,在院团委的指导下,
+学院共有学生会、青年志愿者协会、科学技术协会、新媒体工作室,
+四大学生组织,每年将举办大大小小数十场活动,
+极大地丰富着同学们的课余生活。
+
+![院运会](https://static.myseu.cn/2019-xueyuantuisong/tp1.jpg)
+<p style="text-align:center;font-weight:bold;font-size:14px;">院运会</p>
+![中山陵志愿者活动](https://static.myseu.cn/2019-xueyuantuisong/tp2.png)
+<p style="text-align:center;font-weight:bold;font-size:14px;">中山陵志愿者活动</p>
+![“我爱东大”校史校情知识竞赛](https://static.myseu.cn/2019-xueyuantuisong/tp4.jpg)
+<p style="text-align:center;font-weight:bold;font-size:14px;">“我爱东大”校史校情知识竞赛</p>
+![环九龙湖自行车赛](https://static.myseu.cn/2019-xueyuantuisong/tp3.jpg)
+<p style="text-align:center;font-weight:bold;font-size:14px;">环九龙湖自行车赛</p>
 `
       }
     },
@@ -171,6 +291,7 @@ Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你
       makeResume: async function () {
         await this.progressivelyShowStyle(0)
         await this.progressivelyShowResume()
+        this.interval = 10
         await this.progressivelyShowStyle(1)
         await this.showHtml()
         await this.progressivelyShowStyle(2)
