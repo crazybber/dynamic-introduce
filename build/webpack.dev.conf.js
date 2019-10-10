9a9e7350267aa193cd3b884792193475c1f6725b
eamon <eamon_chen@outlook.com>
2019-10-10 17:28:38 +0800

    update pacakges

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -17,6 +17,9 @@ module.exports = merge(baseWebpackConfig, {
   },
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
+  devServer: {
+ // disableHostCheck:true
+},
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
