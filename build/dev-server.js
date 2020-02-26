db8e23c642e63d739b51c391aff91426f90db597
Edward <chenlulei@gmail.com>
2020-02-26 20:17:40 +0800

    update buiding

--- build/dev-server.js
+++ build/dev-server.js
@@ -62,7 +62,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://0.0.0.0:' + port
+var uri = 'localhost:' + port
 
 devMiddleware.waitUntilValid(function () {
   console.log('> Listening at ' + uri + '\n')
