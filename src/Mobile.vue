e241194cee955641cd58c0018d57a7d7ef72e14b
Edward <crazybber@outlook.com>
2020-05-07 23:07:34 +0800

    move md to md file

--- src/Mobile.vue
+++ src/Mobile.vue
@@ -258,48 +258,7 @@ h3{
 `,
       ],
       currentMarkdown: "",
-      fullMarkdown: `
-
-## Arch.run
-----
-- Mobile：15*********;
-- Wechat：crazybber;
-- Site: https://arch.run;
-
-
-## 基本信息
-- Name: https://arch.run;
-- Education: Never had been to School;
-- Location: SH;
-- Work: Now you see i'm a simle site;
-
-## Skill Sets
-----
-* SVN | GIT | NGINX | APACHE
-* LINUX | windows | MacOS | Android | UOS
-* Kubernets | DOCKER
-* MQ|MQTT|Onvif|Video|Streaming
-* PIP | NPM | WEBPACK | GULP
-* Golang | C# | LARAVEL | SYMFONY
-* Angular | VUE | H5 | NODEJS
-
-
-## Resources
-----
-* [Go Exercise](https://github.com/crazybber/go-fucking-exercise)
-* [Go Patterns](https://github.com/crazybber/go-fucking-patterns)
-
-
-## Organizations
-----
-* [Open Source](http://github.com/gb28181)
-* [Community](https://github.com/micro-in-cn)
-
-## Links
-----
-* [GitHub](https://github.com/crazybber)
-
-  `,
+      fullMarkdown: `loading.... `,
     };
   },
   created() {
@@ -355,7 +314,7 @@ h3{
     },
     progressivelyShowResume() {
       let loadMD = () => {
-        const url = "./content.md";
+        const url = "introduce.md";
         axios.get(url).then((response) => {
           this.fullMarkdown = response.data;
         });
