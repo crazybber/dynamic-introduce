c3f6be14e0725b4cfeb264471ceefcb12d4a78f5
Edward <chenlulei@gmail.com>
2020-05-08 12:18:16 +0800

    update build config :  disableHostCheck:true

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -20,7 +20,7 @@ module.exports = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   devServer: {
- // disableHostCheck:true
+  disableHostCheck:true
 },
   plugins: [
     // make sure to include the plugin for the magic
